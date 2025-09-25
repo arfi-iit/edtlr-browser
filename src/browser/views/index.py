232224749b447a34ebb3ef6fce22930de8fe6272
RePierre <petru.rebeja@gmail.com>
2025-09-25 19:59:29 +0300

    Implemented the post-redirect-get mechanism for searching terms

--- src/browser/views/index.py
+++ src/browser/views/index.py
@@ -1,10 +1,16 @@
 """The index view."""
-from django.views import View
+from django.http import QueryDict
+from django.shortcuts import redirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.views import View
 
 
 class IndexView(View):
     """Implements the index view."""
+
+    template_name = "index.html"
+    index_page = "browser:index"
 
     def get(self, request):
         """Handle the GET request.
@@ -14,4 +20,22 @@ class IndexView(View):
         request: HttpRequest, required
             The request object.
         """
-        return render(request, "index.html")
+        search_term = request.GET.get('t', '')
+        return render(request,
+                      self.template_name,
+                      context={'search_term': search_term})
+
+    def post(self, request):
+        """Handle the POST request.
+
+        Parameters
+        ----------
+        request: HttpRequest, required
+            The request object.
+        """
+        term = request.POST.get('term')
+        if not term:
+            return redirect(self.index_page)
+        params = QueryDict.fromkeys(['t'], value=term)
+        base_url = reverse(self.index_page)
+        return redirect(f'{base_url}?{params.urlencode()}')
